9c622284b87a445e5df9ed83cd6d29ddea0e810c
subanisayed@gmail.com <subanisayed@gmail.com>
2026-01-16 11:24:04 +0530

    MVC proect

---
+++ WebAppAutoDeployInAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppAutoDeployInAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
